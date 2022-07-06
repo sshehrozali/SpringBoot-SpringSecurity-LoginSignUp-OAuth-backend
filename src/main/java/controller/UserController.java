195b6f4e26af43270eaff581a38b5b7e2bf70ef3
Shehroz Ali <sshehroza13@gmail.com>
2022-07-06 14:56:19 +0500

    Added @PostMapping method Added @Autowired Spring bean annonation for dependency injection for User Service

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,7 +1,21 @@
 package controller;
 
+import model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserModel newUserData) {
+
+    }
 }
