ee6ac3ff2221d5a90a79f7b68c01e58ee703725f
Shehroz Ali <sshehroza13@gmail.com>
2022-07-06 14:39:19 +0500

    first push to main

---
+++ src/main/java/com/main/signuploginapp/SignupLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.main.signuploginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupLoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupLoginAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/signuploginapp/SignupLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.signuploginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupLoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
