36962a4ebed4f6e5cf18255dccc7875ac595b492
Shehroz Ali <sshehroza13@gmail.com>
2022-07-06 15:05:37 +0500

    Created all args constructor Created no-args contructor Created Getters and Setters

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -5,11 +5,62 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAccess() {
+        return access;
+    }
+
+    public void setAccess(boolean access) {
+        this.access = access;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String name;
     private String email;
     private String password;
     private boolean access = false;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, boolean access) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.access = access;
+    }
 }
