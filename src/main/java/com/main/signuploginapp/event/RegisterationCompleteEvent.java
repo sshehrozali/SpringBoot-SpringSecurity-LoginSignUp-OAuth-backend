be6112b13824e731395082c93b73ef4115661904
Shehroz Ali <sshehroza13@gmail.com>
2022-07-06 21:46:03 +0500

    Added @Getters and @Setters

--- src/main/java/com/main/signuploginapp/event/RegisterationCompleteEvent.java
+++ src/main/java/com/main/signuploginapp/event/RegisterationCompleteEvent.java
@@ -1,8 +1,12 @@
 package com.main.signuploginapp.event;
 
 import com.main.signuploginapp.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.ApplicationEvent;
 
+@Getter
+@Setter
 public class RegisterationCompleteEvent extends ApplicationEvent {
     private User user;
     private String applicationUrl;
