de8bb4a17899fc9fa2a28edf81a57aebf39e27bf
Shehroz Ali <sshehroza13@gmail.com>
2022-07-06 14:52:58 +0500

    Created User model

---
+++ src/main/java/model/UserModel.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel {
+    private String incoming_email;
+    private String incoming_password;
+}
